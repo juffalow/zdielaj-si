aaeb3dffb331dafc40b95170b0db6ff43f54eb57
Matej Jellus <juffalow@juffalow.com>
2020-10-25 01:21:27 +0200

    Update cors policy

--- backend/src/config.example.ts
+++ backend/src/config.example.ts
@@ -1,10 +1,22 @@
 export default {
   port: 3010,
+  allowedOrigins: [
+    'http://localhost:3000',
+  ],
   storage: {
     url: process.env.STORAGE_URL || '',
     key: process.env.STORAGE_KEY || '',
     endpoint: process.env.STORAGE_ENDPOING || '',
     secret: process.env.STORAGE_SECRET || '',
     bucket: process.env.STORAGE_BUCKET || '',
+  },
+  database: {
+    connection: {
+      database : process.env.DATABASE_NAME || '',
+      host : process.env.DATABASE_HOST || '',
+      password : process.env.DATABASE_PASSWORD || '',
+      user : process.env.DATABASE_USER || '',
+    },
+    runMigrations: true,
   },
 }
