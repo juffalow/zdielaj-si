a0d8426b9977c015f0280e84b1cc3c2b72c29062
Matej Jellus <juffalow@juffalow.com>
2020-10-24 14:11:08 +0200

    Update about page

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import SEO from '../components/SEO';
+import lake from './about/jazero.jpg';
 
 const About: React.FC = () => (
   <SEO title="O aplikacii" description="">
@@ -11,6 +12,9 @@ const About: React.FC = () => (
         <Col>
           <h1>O aplikacii</h1>
           <p className="lead">Bola streda, 21. október a my sme sa ako každú stredu boli otužovať ráno pred prácou v jazere. Hanka, náša hlavá fotografka, spravila pár fotiek ešte kým sme boli vo vode aby nám kamaráti verili.</p>
+          <div className="text-center" style={{ margin: '2em 0' }}>
+            <img src={lake} width="400" />
+          </div>
           <p>Samozrejme, neskôr chcel každý z nás zdielať tieto fotky či už v aktivite na Strave, alebo v storieske na Instagrame. Dostupné boli v spoločnom chate v Slacku, ale kvalita fotky už bola značne znížená. Poslať si fotky medzi sebou, keď sme o miestnosť vedľa nie je problém v prípade, že všetci majú buď iPhone alebo Android. Ale nemajú... A niekto už sedel pekne doma pod perinkou na home office.</p>
           <p>A tu, v týchto zlých, závislákom a zdielačom neprajných časoch sme si povedali, že prídeme s riešením.</p>
           <blockquote className="blockquote text-right mt-4">
