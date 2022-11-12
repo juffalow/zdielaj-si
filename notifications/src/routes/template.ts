3ab5a8d81eb40cfacdbbd2f40e6334a522d7f316
Matej Jellus <juffalow@juffalow.com>
2022-11-12 21:44:51 +0100

    Fix template email route

--- notifications/src/routes/template.ts
+++ notifications/src/routes/template.ts
@@ -1,11 +1,19 @@
 import express from 'express';
-import templates from '../templates/email';
+import logger from '../logger';
 
 const router = express.Router();
 
 router.get('/email/:name', async (req: express.Request, res: express.Response) => {
   const name = req.params.name;
-  const template = templates(name);
+
+  const filename = '../templates/email/' + name.toLowerCase() + '.js';
+  
+  const template = await import(filename)
+    .then(module => module.default)
+    .catch(() => {
+      logger.error(`Template ${name} does not exist!`);
+      throw `Template ${name} does not exist!`;
+    });
 
   const body = template.render(req.query);
   
