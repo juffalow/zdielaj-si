6a1f0eb195229760a99e7e1e8dc4cd044eba41ff
Matej Jellus <juffalow@juffalow.com>
2024-11-12 21:57:04 +0100

    Add sentry for frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as Sentry from '@sentry/react';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [],
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
