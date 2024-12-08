b77a305517edd5dc0c24b1b4af85fcd8b11769ea
Matej Jellus <juffalow@juffalow.com>
2024-12-08 21:02:36 +0100

    User language detector and store language settings in localStorage

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,12 +1,13 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import detector from 'i18next-browser-languagedetector';
 import translationSK from "./locales/sk/translation.json";
 import translationEN from "./locales/en/translation.json";
 import translationCZ from "./locales/cz/translation.json";
 import translationDE from "./locales/de/translation.json";
 import translationES from "./locales/es/translation.json";
 
-i18n.use(initReactI18next).init({
+i18n.use(detector).use(initReactI18next).init({
   lng: 'sk',
   fallbackLng: 'sk',
   interpolation: {
@@ -29,6 +30,7 @@ i18n.use(initReactI18next).init({
       translation: translationES,
     },
   },
+  cache: ['localStorage'],
 });
 
 export default i18n;
