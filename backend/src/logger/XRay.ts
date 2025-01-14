7e215ed2a80cabad914bc82e9da005a92f2f30bc
Matej Jellus <juffalow@juffalow.com>
2025-01-14 16:29:16 +0100

    Disable X-Ray for HTTP requests

--- backend/src/logger/XRay.ts
+++ backend/src/logger/XRay.ts
@@ -1,13 +1,13 @@
 import AWSXRay from 'aws-xray-sdk';
-import http from 'http';
-import https from 'https';
+// import http from 'http';
+// import https from 'https';
 
 AWSXRay.config([AWSXRay.plugins.ECSPlugin]);
 
 AWSXRay.setDaemonAddress(process.env.AWS_XRAY_DAEMON_ADDRESS);
 
-AWSXRay.captureHTTPsGlobal(http);
-AWSXRay.captureHTTPsGlobal(https);
-AWSXRay.capturePromise();
+// AWSXRay.captureHTTPsGlobal(http);
+// AWSXRay.captureHTTPsGlobal(https);
+// AWSXRay.capturePromise();
 
 export default AWSXRay;
