2f37bd5f0c67cd06a0ae28e6f31a61403ea781b4
Matej Jellus <juffalow@juffalow.com>
2025-01-19 02:15:48 +0100

    User X-Ray tracing for HTTP requests

--- backend/src/logger/XRay.ts
+++ backend/src/logger/XRay.ts
@@ -1,11 +1,12 @@
 import AWSXRay from 'aws-xray-sdk';
 import { ServiceDiscoveryClient, DiscoverInstancesCommand, HealthStatusFilter } from '@aws-sdk/client-servicediscovery';
-// import http from 'http';
-// import https from 'https';
+import http from 'http';
+import https from 'https';
 
 AWSXRay.config([AWSXRay.plugins.ECSPlugin]);
 
-// AWSXRay.setDaemonAddress(process.env.AWS_XRAY_DAEMON_ADDRESS || '127.0.0.1:2000');
+AWSXRay.captureHTTPsGlobal(http);
+AWSXRay.captureHTTPsGlobal(https);
 
 if (typeof process.env.AWS_XRAY_SERVICE_NAMESPACE === 'string' && typeof process.env.AWS_XRAY_SERVICE_NAME === 'string') {
   const setDaemonAddress = async () => {
@@ -22,8 +23,6 @@ if (typeof process.env.AWS_XRAY_SERVICE_NAMESPACE === 'string' && typeof process
   
     const result = await client.send(command);
   
-    console.log(result);
-  
     const ip = result.Instances[0].Attributes.AWS_INSTANCE_IPV4;
     const port = result.Instances[0].Attributes.AWS_INSTANCE_PORT;
   
@@ -32,9 +31,5 @@ if (typeof process.env.AWS_XRAY_SERVICE_NAMESPACE === 'string' && typeof process
 
   setDaemonAddress();
 }
-
-// AWSXRay.captureHTTPsGlobal(http);
-// AWSXRay.captureHTTPsGlobal(https);
-// AWSXRay.capturePromise();
 
 export default AWSXRay;
