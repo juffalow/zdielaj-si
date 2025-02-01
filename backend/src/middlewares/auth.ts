66194b9cd2a283586e65e64861e74d5e89f56f87
Matej Jellus <juffalow@juffalow.com>
2025-02-01 14:26:46 +0100

    Fix auth middleware

--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -2,7 +2,7 @@ import type { Request, Response, NextFunction } from 'express';
 import services from '../services';
 
 export default async function auth(req: Request, res: Response, next: NextFunction): Promise<void> {
-  if (req.method !== 'OPTIONS' || 'authorization' in req.headers) {
+  if (req.method !== 'OPTIONS' && 'authorization' in req.headers) {
     const token = req.headers.authorization.replace('Bearer ', '');
     const payload = services.Token.verify(token);
     const data = payload instanceof Promise ? await payload : payload;
