a661cce7777847b568c47b8c70ab9d5f79e8294d
Matej Jellus <juffalow@juffalow.com>
2025-02-08 22:52:38 +0100

    Remove albums list form user detail

--- backend/src/controllers/Users.ts
+++ backend/src/controllers/Users.ts
@@ -1,5 +1,4 @@
 import { NotFoundError } from '../errors/APIError';
-import logger from '../logger';
 
 class Users {
   constructor(
@@ -13,49 +12,15 @@ class Users {
   public async get(id: ID, token?: string): Promise<unknown> {
     const remoteUser = await this.userService.get(token);
     const user = await this.userRepository.get(id);
-    const publicProfile = typeof user.publicProfileId === 'string' ? await this.publicProfileRepository.get(user.publicProfileId) : { id: null, albums: [] };
 
     if (typeof user === 'undefined') {
       throw new NotFoundError('User not found!', 404);
     }
-  
-    const albums = await Promise.all(user.albums.map(async (id) => {
-      try {
-        const album = await this.albumRepository.get(id);
-
-        if (album.files.length === 0) {
-          logger.warn('Album has no files!', { album: { id: album.id }, user: { id} });
-
-          return null;
-        }
-
-        const response = await this.uploadService.getFile(album.files[0]);
-        
-        return {
-          id,
-          compressedId: album.compressedId,
-          name: album.name,
-          publicProfile: publicProfile.albums.includes(album.id) ? { id: publicProfile.id } : null,
-          media:[{
-            id: response.data.file.id,
-            location: response.data.file.location,
-            mimetype: response.data.file.mimetype,
-            thumbnails: response.data.file.thumbnails,
-          }],
-          createdAt: album.createdAt,
-        };
-      } catch (error) {
-        logger.error('Error while fetching album or its files!', { id, error: { message: error.message, stack: error.stack } });
-
-        return null;
-      }
-    }));
 
     return {
       ...user,
       email: remoteUser.email,
       name: remoteUser.name,
-      albums: albums.filter((album) => album !== null),
     };
   }
 }
