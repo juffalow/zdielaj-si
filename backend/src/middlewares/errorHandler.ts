3a6acc361db959886499a814325580aba327b979
Matej Jellus <juffalow@juffalow.com>
2025-02-09 23:49:39 +0100

    Fix import path to HTTP error

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,7 +1,7 @@
 import type { Request, Response, NextFunction } from 'express';
 import APIError from '../errors/APIError';
 import logger from '../logger';
-import { HTTPError } from 'errors/HTTP';
+import { HTTPError } from '../errors/HTTP';
 
 /* eslint-disable  @typescript-eslint/no-unused-vars */
 export default function error(err: Error, req: Request, res: Response, next: NextFunction): void {
