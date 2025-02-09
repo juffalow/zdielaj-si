182f141c9db2c1376ab965da534a93e54fc27821
Matej Jellus <juffalow@juffalow.com>
2025-02-09 23:39:46 +0100

    Improve HTTP error logging

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,10 +1,21 @@
 import type { Request, Response, NextFunction } from 'express';
 import APIError from '../errors/APIError';
 import logger from '../logger';
+import { HTTPError } from 'errors/HTTP';
 
 /* eslint-disable  @typescript-eslint/no-unused-vars */
 export default function error(err: Error, req: Request, res: Response, next: NextFunction): void {
   if (err instanceof APIError) {
+    res.status(err.response.status).json({
+      error: {
+        message: err.message,
+        code: err.code,
+      },
+      data: null,
+    }).end();
+  } else if (err instanceof HTTPError) {
+    logger.error('Something went wrong!', { error: { message: err.message, url: err.request.url, body: err.response.body, stack: err.stack } });
+
     res.status(err.response.status).json({
       error: {
         message: err.message,
