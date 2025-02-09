e88ca510b340e35b395c150cd88f6dec026565b1
Matej Jellus <juffalow@juffalow.com>
2025-02-09 22:51:07 +0100

    Update user albums route to not collide with users service url

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import albums from './albums';
 import notifications from './notifications';
 import publicProfiles from './publicProfiles';
-import usersAlbums from './users/albums';
+import meAlbums from './me/albums';
 import publicProfilesAlbums from './publicProfiles/Albums';
 import me from './me';
 import shortLinks from './shortLinks';
@@ -14,7 +14,7 @@ router.use('/notifications', notifications);
 router.use('/publicprofiles', publicProfiles);
 router.use('/publicprofiles', publicProfilesAlbums);
 router.use('/me', me);
+router.use('/me', meAlbums);
 router.use('/shortlinks', shortLinks);
-router.use('/users', usersAlbums);
 
 export default router;
--- backend/src/routes/me/albums.ts
+++ backend/src/routes/me/albums.ts
@@ -4,7 +4,7 @@ import requireAuth from '../../middlewares/requireAuth';
 
 const router = express.Router();
 
-router.get('/:id/albums', requireAuth, async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+router.get('/albums', requireAuth, async (req: express.Request, res: express.Response, next: express.NextFunction) => {
   try {
     const first = req.query.first ? parseInt(req.query.first as string) : 10;
     const after = req.query.after ? parseInt(req.query.after as string) : 0;
