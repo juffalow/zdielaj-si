f1c128b6833ba9cdc2f74bf9410c5d1995d7ff7f
Matej Jellus <juffalow@juffalow.com>
2025-05-22 20:58:49 +0200

    Fix get short link API call

--- frontend/src/api/shortlinks.ts
+++ frontend/src/api/shortlinks.ts
@@ -4,6 +4,6 @@ import retryPromise from '../utils/retryPromise';
 export async function getShortLink(path: string): Promise<{ albumId: ID }> {
   return retryPromise(() => get<any>(`${process.env.REACT_APP_API_URL}/shortlinks/${path}`), { retries: 8, attempt: 0 })
     .then((response) => {
-      return response.data.album;
+      return response.data.shortLink;
     });
 }
