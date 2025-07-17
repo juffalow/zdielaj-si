7a6dcfbae66cc8eff4849267e349794578ce2eb5
Matej Jellus <juffalow@juffalow.com>
2025-07-17 16:33:37 +0200

    Update about page

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -3,14 +3,10 @@ import { useTranslation } from 'react-i18next';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 import SEO from '../components/SEO';
-import FeaturesList from './home/FeaturesList';
-import awsCognito from './about/aws-cognito.svg';
-import awsS3 from './about/aws-s3.svg';
-import awsMC from './about/aws-mc.svg';
-import awsSQS from './about/aws-sqs.svg';
-import awsSES from './about/aws-ses.svg';
-import awsCF from './about/aws-cf.svg';
+
+import { FaGlobeEurope, FaLock, FaCloud } from 'react-icons/fa';
 
 const About: FunctionComponent = () => {
   const { t } = useTranslation('', { keyPrefix: 'about' });
@@ -21,75 +17,65 @@ const About: FunctionComponent = () => {
         <Row>
           <Col>
             <h1 className="text-center">{t("title")}</h1>
-            <p className="lead">{t("subtitle")}</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col className="text-center">
-            <FeaturesList>
-              <li>Dáta sú uložené v EU, konkretne vo Frankfurte (Nemecko)</li>
-              <li>Všetky dáta sú uložené a prenášané v šifrovanej forme</li>
-              </FeaturesList>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h2 className="text-center">Funkcie</h2>
+            <p className="text-center lead">{t("subtitle")}</p>
           </Col>
         </Row>
         <Row className="mt-5">
-          <Col className="mb-4" lg={4}>
-            <h4>Album</h4>
-            <p>Možnosť vytvárať albumy pre fotky a videá. Každý album môže obsahovať ľubovolný počet médií. Všetky nahrané videá sa transkódujú na formát <i>mp4</i>, aby boli spätne prehrateľné v prehliadači. Pri sťahovaní sa vždy sťahuje originál.</p>
-          </Col>
-          <Col className="mb-4" lg={4}>
-            <h4>Zaheslovať prístup</h4>
-            <p>Vlastník albumu môže svoj album chrániť heslom, ktoré bude vyžadované pre načítanie obsahu.</p>
-          </Col>
-          <Col className="mb-4" lg={4}>
-            <h4>Zdieľať album</h4>
-            <p>Do albumu možu pridávať fotky a videá aj iní používatelia. Album sa dá zdieľať pomocou špeciálneho odkazu, alebo s konkrétnymi registrovanými používateľmi.</p>
-          </Col>
-          <Col className="mb-4" lg={4}>
-            <h4>Verejný profil</h4>
-            <p>Na unikátnom odkaze (<code>/tvojemeno/</code>) vidia iní používatelia tvoje albumy, ktoré patria do daného verejného profilu.</p>
-          </Col>
-        </Row>
-        <Row>
           <Col>
-            <h2 className="text-center">Technológie</h2>
+            <h2 id="public-profile" className="text-center">{t("features.title")}</h2>
           </Col>
         </Row>
-        <Row className="mt-5">
-          <Col className="mb-4" lg={4}>
-            <img src={awsCognito} style={{ height: '3rem', marginBottom: '1rem' }} />
-            <h4>AWS Cognito</h4>
-            <p>Pomáha spravovať autentifikáciu, autorizáciu a správu používateľov pre webové a mobilné aplikácie. Poskytuje silné bezpečnostné opatrenia, vrátane šifrovania dát, multi-faktorovej autentifikácie a možnosti blokovania účtov po niekoľkých neúspešných pokusoch o prihlásenie.</p>
+        <Row className="justify-content-center row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 pt-5">
+          <Col className="d-flex align-items-start">
+            <FaGlobeEurope className="bi text-body-secondary flex-shrink-0 me-3" style={{ fontSize: '1.4rem' }} />
+            <div>
+              <h3 className="mb-0 fs-4 text-body-emphasis">{t("features.euData.title")}</h3>
+              <p className="mt-2">{t("features.euData.subtitle")}</p>
+            </div>
           </Col>
-          <Col className="mb-4" lg={4}>
-            <img src={awsS3} style={{ height: '3rem', marginBottom: '1rem' }} />
-            <h4>AWS S3</h4>
-            <p>Poskytuje úložisko pre ukladanie a načítavanie dát. Má silné bezpečnostné opatrenia, vrátane šifrovania dát, kontrolu prístupu a sledovanie aktivity. Automaticky replikuje dáta na viacerých fyzických miestach, čo znižuje riziko stráty dát.</p>
+          <Col className="d-flex align-items-start">
+            <FaCloud className="bi text-body-secondary flex-shrink-0 me-3" style={{ fontSize: '1.4rem' }} />
+            <div>
+              <h3 className="mb-0 fs-4 text-body-emphasis">{t("features.highlyAvailable.title")}</h3>
+              <p className="mt-2">{t("features.highlyAvailable.subtitle")}</p>
+            </div>
           </Col>
-          <Col className="mb-4" lg={4}>
-            <img src={awsSQS} style={{ width: '3rem', marginBottom: '1rem' }} />
-            <h4>AWS Simple Queue Service</h4>
-            <p>Umožňuje bezproblémové spracovanie veľkého množstva správ. Je navrhnutá tak, aby pomáhala vývojárom vytvárať škálovateľné aplikácie.</p>
+          <Col className="d-flex align-items-start">
+            <FaLock className="bi text-body-secondary flex-shrink-0 me-3" style={{ fontSize: '1.4rem' }} />
+            <div>
+              <h3 className="mb-0 fs-4 text-body-emphasis">{t("features.encryption.title")}</h3>
+              <p className="mt-2">{t("features.encryption.subtitle")}</p>
+            </div>
           </Col>
-          <Col className="mb-4" lg={4}>
-            <img src={awsSES} style={{ width: '3rem', marginBottom: '1rem' }} />
-            <h4>AWS Simple Email Service</h4>
-            <p>Umožňuje odosielať a prijímať e-maily. Je navrhnutá tak, aby pomáhala digitálnym marketérom a vývojárom aplikácií odosielať marketingové, upozorňovacie a transakčné e-maily.</p>
+        </Row> 
+        <Row className="justify-content-center g-4 pt-5">
+          <Col lg={12} md={12}>
+            <h3 className="text-center">{t("features.publicProfile.title")}</h3>
+            <p className="text-center">{t("features.publicProfile.subtitle")}</p>
           </Col>
-          <Col className="mb-4" lg={4}>
-            <img src={awsCF} style={{ width: '3rem', marginBottom: '1rem' }} />
-            <h4>AWS CloudFront</h4>
-            <p>Používa globálnu sieť distribučných bodov, ktoré sú geograficky blízko koncovým používateľom, čo znižuje latenciu a zvyšuje rýchlosť načítania obsahu.</p>
+          <Col lg={4} md={12}>
+            <Card>
+              <Card.Body>
+                <Card.Title>BWL fotky</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">https://zdielaj.si/profil/bwl-fotky</Card.Subtitle>
+                <Card.Text>
+                  {t("features.publicProfile.bwl.description")}
+                </Card.Text>
+                <Card.Link href="https://zdielaj.si/profil/bwl-fotky">{t("features.publicProfile.showProfileButton")}</Card.Link>
+              </Card.Body>
+            </Card>
           </Col>
-          <Col lg={4}>
-            <img src={awsMC} style={{ width: '3rem', marginBottom: '1rem' }} />
-            <h4>AWS MediaConvert</h4>
-            <p>Umožňuje prevádzať mediálne súbory z jedného formátu do druhého. Podporuje širokú škálu vstupných a výstupných formátov, čo umožňuje prevádzať takmer akýkoľvek typ mediálneho súboru.</p>
+          <Col lg={4} md={12}>
+            <Card>
+              <Card.Body>
+                <Card.Title>Elis the Bengal Cat</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">https://zdielaj.si/profil/elis</Card.Subtitle>
+                <Card.Text>
+                  {t("features.publicProfile.elis.description")}
+                </Card.Text>
+                <Card.Link href="https://zdielaj.si/profil/elis">{t("features.publicProfile.showProfileButton")}</Card.Link>
+              </Card.Body>
+            </Card>
           </Col>
         </Row>
       </Container>
