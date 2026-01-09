7c99b79b4f719e7f55b19948d7e1c5f85e27436b
Matej Jellus <juffalow@juffalow.com>
2026-01-09 14:06:03 +0100

    Update logger

--- application/app/logger/index.ts
+++ application/app/logger/index.ts
@@ -1,12 +1,12 @@
 import logger from 'loglevel';
-// import { LogLevelDesc } from 'loglevel';
-// import { getQueryParameter } from '../utils/functions';
+import type { LogLevelDesc } from 'loglevel';
+import { getQueryParameter } from '../utils/functions';
 
-// const queryLevel = getQueryParameter('loglevel');
+const queryLevel = getQueryParameter('loglevel');
+const defaultLevel = process.env.NODE_ENV === 'production' ? 'error' : 'debug';
 
-// const level = (['error', 'warn', 'info', 'debug'].includes(queryLevel) ? queryLevel : 'error') as LogLevelDesc;
+const level = (['error', 'warn', 'info', 'debug'].includes(queryLevel) ? queryLevel : defaultLevel) as LogLevelDesc;
 
-// logger.setLevel(process.env.NODE_ENV === 'production' ? level : 'debug');
-logger.setLevel('debug');
+logger.setLevel(level);
 
 export default logger;
--- application/app/utils/functions.ts
+++ application/app/utils/functions.ts
@@ -1,3 +1,9 @@
+export function getQueryParameter(name: string, defaultValue = ''): string {
+  const urlParams = new URLSearchParams(window.location.search);
+  const value = urlParams.get(name);
+  return value || defaultValue;
+}
+
 export function formatDate(date: Date | string, format = 'YYYY-MM-dd HH:mm:ss'): string {
   if (typeof date !== 'object') {
     date = new Date(Date.parse(date));
